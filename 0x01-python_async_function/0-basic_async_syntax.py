3cf6382e8c107292181ce9dc9932a9e8aa379358
AdonyasG <adonyas2000@gmail.com>
2023-04-03 06:17:16 -0400

    type annotation added

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,6 +9,6 @@ import random
 
 async def wait_random(wait_random: int =10) -> float:
     """simple async"""
-    delay = random.uniform(0, wait_random)
+    delay: float = random.uniform(0, wait_random)
     await asyncio.sleep(delay)
     return delay
