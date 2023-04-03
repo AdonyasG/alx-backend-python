eca1128949df7ec2d22719575d4c390b8e627437
AdonyasG <adonyas2000@gmail.com>
2023-04-03 06:13:07 -0400

    max_delay renamed wait_random

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,8 +7,8 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(wait_random=10):
     """simple async"""
-    delay = random.uniform(0, max_delay)
+    delay = random.uniform(0, wait_random)
     await asyncio.sleep(delay)
     return delay
