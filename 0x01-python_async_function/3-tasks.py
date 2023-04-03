cf3971af677dd0c3d9b0ab80d2813e490de627fa
AdonyasG <adonyas2000@gmail.com>
2023-04-03 08:39:15 -0400

    fix

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -8,7 +8,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: float) -> asyncio.Task:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     returns a Task object that represents the coroutine,
     which you can then use to monitor the status of
