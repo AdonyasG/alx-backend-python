6dfa2eec92ef01e7451968d682ee309c62c6440e
AdonyasG <adonyas2000@gmail.com>
2023-04-03 08:24:54 -0400

    initial commit

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module - tasks
+"""
+
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: float) -> asyncio.Task:
+    """
+    returns a Task object that represents the coroutine,
+    which you can then use to monitor the status of
+    the coroutine and potentially cancel it
+    """
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
