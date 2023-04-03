d4a817cfbeeff68d47156ae80874d872d48aa17a
AdonyasG <adonyas2000@gmail.com>
2023-04-03 06:09:46 -0400

    define variable for delay

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,5 +9,6 @@ import random
 
 async def wait_random(max_delay=10):
     """simple async"""
-    await asyncio.sleep(random.uniform(0, max_delay))
-    return random.uniform(0, max_delay)
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
