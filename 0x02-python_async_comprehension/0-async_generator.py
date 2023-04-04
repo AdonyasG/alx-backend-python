6281ddf4a53f070e2ac3571256202638ecbf7752
AdonyasG <adonyas2000@gmail.com>
2023-04-04 08:16:47 -0400

    initial commit

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module - async_generator
+"""
+import asyncio
+import random
+
+
+async def async_generator() -> float:
+    """async generator"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
