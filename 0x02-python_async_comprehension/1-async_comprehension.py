24607d0662620c2d2f84aa786ae55e0dfca05eaa
AdonyasG <adonyas2000@gmail.com>
2023-04-04 09:01:55 -0400

    type fix

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,13 +2,12 @@
 """
 Module - async_comprehension
 """
-import asyncio
 from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List:
+async def async_comprehension() -> List[float]:
     """async comprehension"""
     result = [i async for i in async_generator()]
     return result
