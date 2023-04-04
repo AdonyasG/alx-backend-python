404a1bf7159f7af475dccde80e83f4409c1319f3
AdonyasG <adonyas2000@gmail.com>
2023-04-04 08:59:55 -0400

    fix

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,11 +3,12 @@
 Module - async_comprehension
 """
 import asyncio
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List:
     """async comprehension"""
     result = [i async for i in async_generator()]
     return result
