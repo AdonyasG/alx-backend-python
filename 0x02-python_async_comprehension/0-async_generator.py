65b08564b86a12ef751d0584ae99f33dfda3dec9
AdonyasG <adonyas2000@gmail.com>
2023-04-04 08:27:59 -0400

    type annotaion for return fix

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,11 @@ Module - async_generator
 """
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator() -> float:
+async def async_generator() -> AsyncGenerator[float, None]:
     """async generator"""
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random()
