b6efebaa1ec04a9d3afd9d8ea6183251f9e38749
AdonyasG <adonyas2000@gmail.com>
2023-04-24 17:17:46 -0400

    mock http

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -29,5 +29,23 @@ class TestAccessNestedMap(unittest.TestCase):
             utils.access_nested_map(nested_map, path)
 
 
+class TestGetJson(unittest.TestCase):
+    """test class"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """mock http"""
+        with unittest.mock.patch('requests.get') as mock_json:
+            mock_json.return_value = unittest.mock.Mock()
+            mock_json.return_value.json.return_value = test_payload
+
+            result = utils.get_json(test_url)
+
+            mock_json.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
+
+
 if __name__ == '__main__':
     unittest.main()
