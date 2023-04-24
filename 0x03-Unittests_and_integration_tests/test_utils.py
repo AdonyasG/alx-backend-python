7ad95775e8b729e2775a06c01caf804c8e5feda8
AdonyasG <adonyas2000@gmail.com>
2023-04-24 16:17:32 -0400

    test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,7 +9,6 @@ from parameterized import parameterized
 
 class TestAccessNestedMap(unittest.TestCase):
     """test class"""
-
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -19,6 +18,17 @@ class TestAccessNestedMap(unittest.TestCase):
         """tests access nested map"""
         result = utils.access_nested_map(nested_map, path)
         self.assertEqual(result, expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """key check and raise error"""
+        with self.assertRaises(KeyError) as C:
+            utils.access_nested_map(nested_map, path)
+
+        self.assertEqual((C.exception), "KeyError: '{}'".format(path[0]))
 
 
 if __name__ == '__main__':
